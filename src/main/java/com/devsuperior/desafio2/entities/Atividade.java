95fe0e6b2e94d8b899ad4966b6ca46d81e0e5f03
Eduardo Serafim <eduserafim7@gmail.com>
2023-06-12 22:19:09 -0300

    Repositório Criado

---
+++ src/main/java/com/devsuperior/desafio2/entities/Atividade.java
@@ -0,0 +1,83 @@
+package com.devsuperior.desafio2.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private Double preco;
+
+    @ManyToMany
+    @JoinTable(name = "tb_participante_atividade",
+                joinColumns = @JoinColumn(name = "atividade_id"),
+                inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_bloco",
+                joinColumns = @JoinColumn(name = "atividade_id"),
+                inverseJoinColumns = @JoinColumn(name = "bloco_id"))
+    private Set<Bloco> blocos = new HashSet<>();
+
+
+    public Atividade() {
+    }
+
+    public Atividade(Long id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+}
